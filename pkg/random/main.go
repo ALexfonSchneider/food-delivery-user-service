91baf4405641811a998340327a5db9311a0d4847
alexxschh <alexxschh@gmail.com>
2025-05-14 11:51:18 +0700

    feat: пример интеграционного теста

---
+++ pkg/random/main.go
@@ -0,0 +1,13 @@
+package random
+
+import "math/rand/v2"
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.IntN(len(letterRunes)-1)]
+	}
+	return string(b)
+}
